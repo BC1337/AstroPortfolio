3d653168c0d91d07f7715efa2aa5ddea6795e92d
BC1337 <brettcomrie@gmail.com>
2024-06-03 08:35:40 -0500

    Fix: Refactored product card so all buttons are aligned, refacorted index.astro due to issue with images shrinking on mobile screen sizes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,6 +8,10 @@ const ProjectCard = ({ title, screenshot, description, demoHref, demoText }) => 
     borderRadius: '5px',
     transition: 'background-color 0.3s ease',
     margin: '10px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%',
   };
 
   const linkStyles = {
@@ -17,20 +21,24 @@ const ProjectCard = ({ title, screenshot, description, demoHref, demoText }) => 
     borderRadius: '5px',
     textDecoration: 'none',
     transition: 'background-color 0.3s ease',
-    marginTop: '8px',
-    marginBottom: '8px',
+    marginTop: 'auto',
+    marginBottom: '8px', // Adjusted space between description and button
   };
 
-  const lightModeStyles = {
-    color: 'white', // Set text color to light in light mode
+  const imageStyles = {
+    backgroundImage: `url(${screenshot})`,
+    backgroundSize: 'cover',
+    backgroundPosition: title === 'Fullstack Auth App' ? 'top' : 'center', // Adjust background position for Fullstack Auth App
+    height: '100%',
+    minHeight: '200px',
   };
 
   return (
-    <div className="project-card" style={{ ...cardStyles, ...lightModeStyles }}>
-      <div className="project-screenshot" style={{ backgroundImage: `url(${screenshot})`, backgroundSize: 'cover', height: '200px' }}></div>
-      <div className="project-content" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-        <h3 className="project-title" style={{ margin: '8px 0', ...lightModeStyles }}>{title}</h3>
-        <p className="project-description" style={{ margin: '8px 0', ...lightModeStyles }}>{description}</p>
+    <div className="project-card" style={cardStyles}>
+      <div className="project-screenshot" style={imageStyles}></div>
+      <div className="project-content" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}>
+        <h3 className="project-title" style={{ margin: '8px 0' }}>{title}</h3>
+        <p className="project-description" style={{ margin: '4px 0' }}>{description}</p> {/* Reduce the top and bottom margin for the description */}
         <a href={demoHref} className="project-link" style={linkStyles} target="_blank" rel="noopener noreferrer">
           {demoText}
         </a>
